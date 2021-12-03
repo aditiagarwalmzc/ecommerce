a7fe2713d0171e26a5de021f1f0fbeab89015288
aditiagarwalmzc <btech25075.20@bitmesra.ac.in>
2021-12-03 07:36:46 +0530

    react router and footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import Directory from './components/Directory';
+import { Switch, Route } from 'react-router-dom';
+
+//layouts
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+
+//pages
 import Header from './components/Header';
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
 import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+          <Route exact path="/" render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )} />
+          <Route path="/registration" render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+const Footer = props => {
+    return (
+        <footer className="footer">
+            <div classname="wrap">
+            © Evanelle 2021, courtesy of Mig a.k.a Aditi
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
